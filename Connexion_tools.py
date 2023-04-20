20cf2f2f512599496ef219261bee02976f1df9ef
kurotix <35796644+kurotix@users.noreply.github.com>
2023-04-20 15:55:48 +0200

    Get token puis get contenu build

--- Connexion_tools.py
+++ Connexion_tools.py
@@ -1,22 +1,66 @@
 import requests
 import json
-from base64 import b64encode
+import base64
+import sys
 
-# Définir les informations d'authentification
-username = input("Entrez votre nom d'utilisateur : ")
-password = input("Entrez votre mot de passe : ")
+# Récupération des arguments passés lors de l'exécution du script
+if len(sys.argv) != 3:
+    print("Usage: python script.py <username> <password>")
+    sys.exit(1)
 
-# URL pour générer un token
-token_url = "https://toto/api/v1/tokens/generate"
+username = sys.argv[1]
+password = sys.argv[2]
 
-# Créer le corps de la demande avec les informations d'authentification
-auth_data = {
-    "username": username,
-    "password": password
+# Certificat SSL
+cert_file = "cert.crt"
+
+# URL de base de l'API
+base_url = "https://toto/api/v1"
+
+# Endpoint pour générer un token
+token_endpoint = base_url + "/tokens/generate"
+
+# Endpoint pour la requête GET après authentification avec le token
+builds_endpoint = base_url + "/builds/16463"
+
+# Encodage en base64 de l'authentification
+auth = base64.b64encode(f"{username}:{password}".encode()).decode()
+
+# En-têtes de la requête pour générer le token
+headers = {
+    "Content-Type": "application/x-www-form-urlencoded",
+    "Authorization": f"Basic {auth}"
 }
 
-# Envoyer la requête POST pour générer un token
-response = requests.post(token_url, data=auth_data, verify="certif.crt")
+# Corps de la requête pour générer le token
+data = {
+    "grant_type": "client_credentials"
+}
 
-# Afficher la réponse JSON
-print(json.dumps(response.json(), indent=2))
+# Envoi de la requête pour générer le token
+response = requests.post(token_endpoint, headers=headers, data=data, cert=cert_file)
+
+# Vérification du code de réponse HTTP
+if response.status_code != 200:
+    print(f"Error: {response.status_code} {response.reason}")
+    sys.exit(1)
+
+# Récupération du token dans le corps de la réponse
+token = response.json()["access_token"]
+print("Token: ", token)
+
+# En-têtes de la requête pour la requête GET après authentification avec le token
+headers = {
+    "Authorization": f"Bearer {token}"
+}
+
+# Envoi de la requête GET après authentification avec le token
+response = requests.get(builds_endpoint, headers=headers, cert=cert_file)
+
+# Vérification du code de réponse HTTP
+if response.status_code != 200:
+    print(f"Error: {response.status_code} {response.reason}")
+    sys.exit(1)
+
+# Affichage de la réponse en format JSON
+print(json.dumps(response.json(), indent=4))
