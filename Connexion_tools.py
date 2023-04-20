2cff89aa910688c6d9a9c0d65cd6b60b6a8bb09d
kurotix <35796644+kurotix@users.noreply.github.com>
2023-04-20 15:48:45 +0200

    intégration dans le corps msg

--- Connexion_tools.py
+++ Connexion_tools.py
@@ -9,13 +9,14 @@ password = input("Entrez votre mot de passe : ")
 # URL pour générer un token
 token_url = "https://toto/api/v1/tokens/generate"
 
-# Créer l'en-tête pour l'authentification de base
-auth_header = {
-    "Authorization": "Basic " + b64encode(f"{username}:{password}".encode()).decode()
+# Créer le corps de la demande avec les informations d'authentification
+auth_data = {
+    "username": username,
+    "password": password
 }
 
 # Envoyer la requête POST pour générer un token
-response = requests.post(token_url, headers=auth_header, verify="certif.crt")
+response = requests.post(token_url, data=auth_data, verify="certif.crt")
 
 # Afficher la réponse JSON
 print(json.dumps(response.json(), indent=2))
