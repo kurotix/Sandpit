70704c51a99cf3823657401e0bacb7bfcf712e1e
kurotix <35796644+kurotix@users.noreply.github.com>
2023-04-27 15:10:22 +0200

    Upgrade

--- Catalogue.py
+++ Catalogue.py
@@ -1,34 +1,27 @@
 import requests
-from bs4 import BeautifulSoup
-from selenium import webdriver
 
-# Paramètres de connexion
-username = "kurotix"
-password = "toto78"
+# Ignorer les erreurs de certificat SSL auto-signé
+requests.packages.urllib3.disable_warnings()
 
 # Créer une session pour maintenir l'état de connexion
 session = requests.Session()
 
-# Se connecter
+# Ignorer les erreurs de certificat SSL pour toutes les requêtes
+session.verify = False
+
+# Se connecter et accéder à la page de téléchargement
 login_url = "https://toto.fr/login"
-login_page = session.get(login_url)
-soup = BeautifulSoup(login_page.content, "html.parser")
-csrf_token = soup.find("input", {"name": "csrf_token"})["value"]
+download_url = "https://toto.fr/visuel"
+username = "kurotix"
+password = "toto78"
+
 payload = {
     "username": username,
     "password": password,
-    "csrf_token": csrf_token,
 }
+
 session.post(login_url, data=payload)
+download_response = session.get(download_url)
 
-# Charger la page de téléchargement
-download_url = "https://toto.fr/visuel"
-driver = webdriver.Chrome()  # utilisez le navigateur de votre choix
-driver.get(download_url)
-
-# Trouver le bouton de téléchargement et le déclencher
-download_button = driver.find_element_by_css_selector(".download-report")
-download_button.click()
-
-# Fermer le navigateur
-driver.quit()
+# Télécharger le contenu du fichier JSON
+json_content = download_response.content
