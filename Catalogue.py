227b8a194f4d6cbdb359a8c375851cfc2ba6cfef
kurotix <35796644+kurotix@users.noreply.github.com>
2023-04-27 15:14:32 +0200

    Rollback si nécessaire

--- Catalogue.py
+++ Catalogue.py
@@ -1,13 +1,8 @@
 import requests
-
-# Ignorer les erreurs de certificat SSL auto-signé
-requests.packages.urllib3.disable_warnings()
+from bs4 import BeautifulSoup
 
 # Créer une session pour maintenir l'état de connexion
 session = requests.Session()
-
-# Ignorer les erreurs de certificat SSL pour toutes les requêtes
-session.verify = False
 
 # Se connecter et accéder à la page de téléchargement
 login_url = "https://toto.fr/login"
@@ -15,13 +10,28 @@ download_url = "https://toto.fr/visuel"
 username = "kurotix"
 password = "toto78"
 
+# Récupérer le token CSRF
+login_response = session.get(login_url)
+soup = BeautifulSoup(login_response.content, "html.parser")
+csrf_token = soup.find("input", {"name": "csrf_token"})["value"]
+
+# Soumettre le formulaire de connexion
 payload = {
     "username": username,
     "password": password,
+    "csrf_token": csrf_token
 }
 
 session.post(login_url, data=payload)
+
+# Télécharger le fichier JSON
 download_response = session.get(download_url)
 
-# Télécharger le contenu du fichier JSON
-json_content = download_response.content
+# Vérifier si le téléchargement est réussi
+if download_response.status_code == 200:
+    # Enregistrer le fichier JSON
+    with open("report.json", "wb") as f:
+        f.write(download_response.content)
+    print("ok")
+else:
+    print("non ok")
