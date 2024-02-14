f592ac1c270203e189c3c3a00757145e16d7eb0e
kurotix <35796644+kurotix@users.noreply.github.com>
2024-02-14 13:57:02 +0100

    Update monitoring_script.ps1

--- monitoring_script.ps1
+++ monitoring_script.ps1
@@ -1,9 +1,7 @@
-# Définir les informations d'identification pour l'accès à l'API Jenkins
+# Définir les informations d'identification pour accéder à Jenkins
 $jenkinsUsername = "votre_nom_utilisateur"
-$jenkinsAPIToken = "votre_token_api"  # Ou votre mot de passe si vous utilisez un mot de passe au lieu d'un token
-
-# Créer les informations d'identification
-$jenkinsCredential = New-Object System.Management.Automation.PSCredential ($jenkinsUsername, (ConvertTo-SecureString -String $jenkinsAPIToken -AsPlainText -Force))
+$jenkinsPassword = ConvertTo-SecureString "votre_mot_de_passe" -AsPlainText -Force
+$jenkinsCredential = New-Object System.Management.Automation.PSCredential ($jenkinsUsername, $jenkinsPassword)
 
 # Définir l'URL de l'API Jenkins pour obtenir l'état du nœud
 $jenkinsNodeAPI = "https://toto-devops.group/p-1451-ilyes/api/json"
@@ -31,6 +29,13 @@ function CheckJenkinsNodeStatus {
     }
 }
 
+# Fonction pour redémarrer le service sur la VM
+function RestartVMService {
+    # Ajoutez ici le code pour redémarrer le service sur votre VM
+    # Par exemple :
+    Restart-Service -Name "nom_du_service" -Force
+}
+
 # Boucle principale
 while ($true) {
     $nodeStatus = CheckJenkinsNodeStatus
@@ -39,8 +44,7 @@ while ($true) {
             Write-Output "Le nœud Jenkins est en ligne et a le label 'windows_58'. Tout est bon."
         } else {
             Write-Output "Le nœud Jenkins est hors ligne ou n'a pas le bon label. Redémarrage des services..."
-            # Ici, vous pouvez appeler une fonction pour redémarrer le service sur la VM
-            # Remarque : cette partie dépendra de la manière dont vous gérez le redémarrage du service sur votre VM
+            RestartVMService
         }
     }
     
