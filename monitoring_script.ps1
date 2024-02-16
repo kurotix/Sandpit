58d3dfe6ee9a5782cf924c124b9764bcb3d9dcbb
kurotix <35796644+kurotix@users.noreply.github.com>
2024-02-16 13:59:59 +0100

    Update monitoring_script.ps1

--- monitoring_script.ps1
+++ monitoring_script.ps1
@@ -1,9 +1,18 @@
 # Définir l'URL de l'API Jenkins
 $jenkinsURL = "https://toto.group/p-2724-ilyes/api/json"
 
+# Définir le nom d'utilisateur et le token d'API
+$jenkinsUsername = "votre_nom_utilisateur"
+$jenkinsAPIToken = "votre_token_api"
+
 try {
+    # Créer l'en-tête d'authentification avec le nom d'utilisateur et le token d'API
+    $headers = @{
+        Authorization = "Basic " + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes("$($jenkinsUsername):$($jenkinsAPIToken)"))
+    }
+
     # Exécuter une requête HTTP GET vers l'API Jenkins pour obtenir l'état du nœud
-    $response = Invoke-RestMethod -Uri $jenkinsURL -Method Get
+    $response = Invoke-RestMethod -Uri $jenkinsURL -Headers $headers -Method Get
 
     # Vérifier si le nœud est présent dans le bloc "assignedLabels" avec le nom "windows_58"
     $nodeFound = $false
