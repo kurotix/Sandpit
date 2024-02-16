195fd41152583cb136d83b397c2ceca5999fcd71
kurotix <35796644+kurotix@users.noreply.github.com>
2024-02-16 14:20:23 +0100

    Update monitoring_script.ps1

--- monitoring_script.ps1
+++ monitoring_script.ps1
@@ -1,5 +1,5 @@
 # Définir l'URL de l'API Jenkins
-$jenkinsURL = "https://toto.group/p-2724-ilyes/api/json"
+$jenkinsURL = "https://toto.group/p-2724-ilyes/computer/api/json"
 
 # Définir le nom d'utilisateur et le token d'API
 $jenkinsUsername = "votre_nom_utilisateur"
@@ -11,12 +11,19 @@ try {
         Authorization = "Basic " + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes("$($jenkinsUsername):$($jenkinsAPIToken)"))
     }
 
-    # Exécuter une requête HTTP GET vers l'API Jenkins pour obtenir l'état du nœud
+    # Exécuter une requête HTTP GET vers l'API Jenkins pour obtenir la liste des ordinateurs
     $response = Invoke-RestMethod -Uri $jenkinsURL -Headers $headers -Method Get
 
-    # Afficher la valeur de "DisplayName"
-    $displayName = $response.displayName
-    Write-Output "La valeur de 'DisplayName' est : $displayName"
+    # Parcourir les ordinateurs pour trouver celui avec "windows_58" comme DisplayName
+    $windowsNode = $response.computer | Where-Object { $_.displayName -eq "windows_58" }
+
+    if ($windowsNode) {
+        # Vérifier si le nœud est en ligne (offline: false)
+        $offlineStatus = $windowsNode.offline
+        Write-Output "Le nœud Jenkins avec le label 'windows_58' est en ligne: $($offlineStatus -eq $false)"
+    } else {
+        Write-Output "Le nœud Jenkins avec le label 'windows_58' n'a pas été trouvé."
+    }
 
 } catch {
     # En cas d'erreur lors de la requête
