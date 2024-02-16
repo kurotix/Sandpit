6e0fa8dab9f0e47ba85efa47a32c09bd01206a94
kurotix <35796644+kurotix@users.noreply.github.com>
2024-02-16 14:23:05 +0100

    Update monitoring_script.ps1

--- monitoring_script.ps1
+++ monitoring_script.ps1
@@ -5,6 +5,9 @@ $jenkinsURL = "https://toto.group/p-2724-ilyes/computer/api/json"
 $jenkinsUsername = "votre_nom_utilisateur"
 $jenkinsAPIToken = "votre_token_api"
 
+# Définir le nom du service Jenkins à redémarrer
+$serviceToRestart = "jenkins-slave-p-2725-check"
+
 try {
     # Créer l'en-tête d'authentification avec le nom d'utilisateur et le token d'API
     $headers = @{
@@ -20,7 +23,16 @@ try {
     if ($windowsNode) {
         # Vérifier si le nœud est en ligne (offline: false)
         $offlineStatus = $windowsNode.offline
-        Write-Output "Le nœud Jenkins avec le label 'windows_58' est en ligne: $($offlineStatus -eq $false)"
+
+        if (-not $offlineStatus) {
+            Write-Output "Le nœud Jenkins avec le label 'windows_58' est en ligne."
+        } else {
+            Write-Output "Le nœud Jenkins avec le label 'windows_58' n'est pas en ligne. Redémarrage du service $serviceToRestart."
+
+            # Redémarrer le service Jenkins si le nœud n'est pas en ligne
+            Restart-Service -Name $serviceToRestart
+            Write-Output "Le service $serviceToRestart a été redémarré."
+        }
     } else {
         Write-Output "Le nœud Jenkins avec le label 'windows_58' n'a pas été trouvé."
     }
