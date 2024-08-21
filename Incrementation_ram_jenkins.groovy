f0749ab779eb1b5663e1d3c0d99029f94c994b21
kurotix <35796644+kurotix@users.noreply.github.com>
2024-08-21 11:53:34 +0200

    Update Incrementation_ram_jenkins.groovy

--- Incrementation_ram_jenkins.groovy
+++ Incrementation_ram_jenkins.groovy
@@ -1,5 +1,4 @@
 import com.cloudbees.opscenter.server.model.ManagedMaster
-import java.util.regex.*
 
 // Liste des noms des masters Jenkins à modifier
 def mastersToModify = ["p-ilyes-78", "p-nassim-78"]
@@ -8,36 +7,30 @@ def mastersToModify = ["p-ilyes-78", "p-nassim-78"]
 Jenkins.instance.getAllItems(ManagedMaster.class).each { masterInstance ->
     def nameInstance = masterInstance.fullName
     if (mastersToModify.contains(nameInstance)) {
-        changeRequestsMemory(masterInstance)
+        changeMasterMemory(masterInstance)
     }
 }
 
-def changeRequestsMemory(def managedController) {
+def changeMasterMemory(def managedController) {
     def instance = jenkins.model.Jenkins.instanceOrNull.getItemByFullName(managedController.fullName, ManagedMaster.class)
     
     if (instance != null) {
         def configuration = instance.getConfiguration()
         if (configuration != null) {
-            def yaml = configuration.getYaml()
-
-            // Trouver et mettre à jour la valeur de la mémoire
-            Matcher subMatcher = Pattern.compile("memory:\\s*\"([0-9]+)\\.000000[MmGg]\"").matcher(yaml)
-            if (subMatcher.find()) {
-                def memoryReq = subMatcher.group(1).toInteger()
-                def newMemoryReq = memoryReq + 2048  // Ajouter 2 Go à la valeur actuelle
-                
-                // Remplacer l'ancienne valeur par la nouvelle
-                def yamlNew = yaml.replaceAll("memory:\\s*\"[0-9]+\\.000000[MmGg]\"", "memory: \"${newMemoryReq}.000000M\"")
-                
-                // Appliquer la nouvelle configuration YAML
-                configuration.setYaml(yamlNew)
-                instance.setConfiguration(configuration)
-                instance.save()
-                
-                println "RAM pour le master '${managedController.fullName}' mise à jour avec succès à ${newMemoryReq}M."
-            } else {
-                println "Aucune configuration de RAM trouvée pour '${managedController.fullName}'."
-            }
+            def provisioning = configuration.getProvisioning() // Accès à la section Provisioning
+            
+            // Supposons que la propriété "jenkinsMasterMemoryMb" soit la bonne, mais vous pouvez ajuster le nom de la propriété selon votre configuration.
+            def currentMemory = provisioning.jenkinsMasterMemoryMb
+            println "Mémoire actuelle pour '${managedController.fullName}': ${currentMemory}MB"
+            
+            def newMemory = currentMemory + 2048  // Ajouter 2 Go (2048 Mo) à la valeur actuelle
+            
+            provisioning.jenkinsMasterMemoryMb = newMemory  // Mettre à jour la valeur
+            
+            instance.setConfiguration(configuration)
+            instance.save()
+            
+            println "RAM pour le master '${managedController.fullName}' mise à jour avec succès à ${newMemory}MB."
         }
     }
 }
