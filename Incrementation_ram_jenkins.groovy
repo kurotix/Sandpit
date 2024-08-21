2ff59abd7e7b25c0237aea71c9f5f286d9ccf5bb
kurotix <35796644+kurotix@users.noreply.github.com>
2024-08-21 15:11:18 +0200

    Update Incrementation_ram_jenkins.groovy

--- Incrementation_ram_jenkins.groovy
+++ Incrementation_ram_jenkins.groovy
@@ -2,27 +2,27 @@ import requests
 from bs4 import BeautifulSoup
 
 # URL de connexion
-login_url = 'https://toto.echonet/cjoc/login'  # Remplacez par l'URL de connexion réelle
-target_url = 'https://toto.echonet/cjoc/p-ilyes-78/configure'  # URL de la page à accéder après connexion
+login_url = 'https://toto.echonet/cjoc/login'
+target_url = 'https://toto.echonet/cjoc/p-ilyes-78/configure'
 
 # Créer une session
 session = requests.Session()
 
-# Les données de connexion (modifiez en fonction du formulaire de connexion)
+# Les données de connexion
 login_data = {
-    'username': 'your-username',  # Remplacez par votre nom d'utilisateur
-    'password': 'your-password',  # Remplacez par votre mot de passe
+    'username': 'your-username',
+    'password': 'your-password',
 }
 
-# Effectuer la connexion
-response = session.post(login_url, data=login_data)
+# Effectuer la connexion avec verification SSL désactivée
+response = session.post(login_url, data=login_data, verify=False)
 
 # Vérifier si la connexion a réussi
 if response.ok:
     print("Connexion réussie")
 
     # Accéder à la page protégée
-    response = session.get(target_url)
+    response = session.get(target_url, verify=False)
 
     # Vérifier si la demande a réussi
     if response.ok:
