aa26efec86dffd6e7b703e543b2c8323f442765e
kurotix <35796644+kurotix@users.noreply.github.com>
2024-08-21 16:36:02 +0200

    Update Incrementation_ram_jenkins.groovy

--- Incrementation_ram_jenkins.groovy
+++ Incrementation_ram_jenkins.groovy
@@ -1,6 +1,39 @@
 import com.cloudbees.opscenter.server.model.ManagedMaster
 
-// Lister tous les Managed Masters gérés par CJOC
-Jenkins.instance.getAllItems(ManagedMaster.class).each { master ->
-    println "Managed Master Name: ${master.name}"
+// Fonction pour augmenter la mémoire
+def increaseMemory(configXml, increment) {
+    return configXml.replaceAll(/-Xmx(\d+)g/, { match, x -> "-Xmx${x.toInteger() + increment}g" })
 }
+
+// Liste des Managed Masters à modifier
+def masters = ["p-ilyes-78", "p-nassim-78"]
+def increment = 2 // Ajouter 2G de RAM
+
+masters.each { masterName ->
+    def master = Jenkins.instance.getAllItems(ManagedMaster.class).find { it.name == masterName }
+
+    if (master != null) {
+        println "Modifying Master: ${master.name}"
+
+        // Obtenir la configuration actuelle du Managed Master
+        def configXml = master.getConfigFile().asString()
+        println "Current Config for ${masterName}: \n$configXml"
+
+        // Augmenter la mémoire de 2G
+        def updatedConfigXml = increaseMemory(configXml, increment)
+
+        // Appliquer la nouvelle configuration
+        master.getConfigFile().write(updatedConfigXml)
+        master.save()
+
+        println "Updated Memory for ${masterName}."
+
+        // Optionnel : Redémarrer le Managed Master pour appliquer les changements
+        master.doSafeRestart()
+        println "Master ${masterName} is restarting..."
+    } else {
+        println "No such master found: ${masterName}"
+    }
+}
+
+println "Memory update completed for selected masters."
